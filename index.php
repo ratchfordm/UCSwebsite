874716106f7c5a8e89c321251d0060ea1d1c94a6
John McGovern <mcgovernj@etown.edu>
2024-10-17 08:04:08 -0400

    Initializing multiple stub files

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INDEX PAGE</title>
-</head>
-<body>
-    <?php
-        echo "Hello, World";
-    ?>
-</body>
-</html>
+<?php
+
+    // No chilling in the source directory
+    header("Location:web_src/index.php")
+
+?>
---
+++ web_src/index.php
@@ -0,0 +1,6 @@
+<?php
+
+    echo "Epic home page.";
+    echo "<br>Website coming soon!";
+
+?>
