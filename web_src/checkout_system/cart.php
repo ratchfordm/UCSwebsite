5477b2825ddcd27f723a9c59322618a3065de97b
Martin Ratchford <ratchfordm@etown.edu>
2024-10-17 08:03:18 -0400

    cart and checkout changes

---
+++ web_src/checkout_system/cart.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping Cart</title>
+</head>
+<body>
+    <h1>Checkout Cart</h1>
+    <p>Scan items with the barcode reader to add them to the cart.</p>
+
+
+
+    <div>Ready to Checkout? Review Items <a href="checkout.php" class="button">Here</a></div>
+    
+</body>
+</html>
