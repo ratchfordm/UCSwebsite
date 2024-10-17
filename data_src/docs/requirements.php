3a2461b96c853d4ee568ea042fff7d956586514d
John McGovern <mcgovernj@etown.edu>
2024-10-17 08:48:22 -0400

    Putting requirements on thet website

---
+++ data_src/docs/requirements.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Requirements</title>
+</head>
+<body>
+    <h2>UCS Project Requirements</h3>
+    <p>For a minimum viable project:</p>
+    <ul>
+        <li>Database containing information on UCS inventory.</li>
+        <li>A system that allows users to place inventory into a cart.</li>
+        <li>A checkout system that allows for the purchase of the user's cart and also updates the database.</li>
+        <l1>An admin setup that allows for event management, as well as general site maintenance.</li>
+    </ul>
+    <p>For a fully realized project:
+    <ul>
+        <li>All of the above.</li>
+        <li>Website should be mobile friendly.</li>
+        <li>An additional, higher-level admin that has control of other admin accounts.</li>
+        <li>Other tasks as discovered.</li>
+    </ul>
+    <br><button type="button" onclick="location.href='../..';">Return</button>
+</body>
+</html>
