665ef8c3cdb7a0b7de7bd81f35590d01e8e1ba89
Martin Ratchford <ratchfordm@etown.edu>
2024-10-17 10:25:09 -0400

    checkout update

--- web_src/checkout_system/cart.php
+++ web_src/checkout_system/cart.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shopping Cart</title>
+
+    <link rel="stylesheet" href="../css/checkout.css">
 </head>
 <body>
     <h1>Checkout Cart</h1>
     <p>Scan items with the barcode reader to add them to the cart.</p>
+
+    <label>Item ID: </label><input>
 
     <table id="shopping_cart">
         <!-- Table Head -->
@@ -19,17 +23,25 @@
 
         <!-- Table Body -->
         <tbody>
-            <tr>
 
-            </tr>
+            <?php
+                // accessing the database
+                require_once "../../data_src/db_config.php";
 
-            <tr>
+                mysql_select_db("db") or die;
+                $results = mysql_query("SELECT item_id, title, price FROM items");
+                while($row = mysql_fetch_array($results)) {
+            ?>
+                <!-- TODO: only update table with scanned items -->
+                <tr>
+                    <td><?php echo $row['item_id']?></td>
+                    <td><?php echo $row['title']?></td>
+                    <td><?php echo $row['price']?></td>
+                </tr>
 
-            </tr>
-
-            <tr>
-
-            </tr>
+            <?php
+                }
+            ?>
         </tbody>
 
     </table>    
@@ -39,3 +51,5 @@
     
 </body>
 </html>
+
+<!-- make js thing that detects when item is scanned, sends to database, gets item that was scanned from database -->
