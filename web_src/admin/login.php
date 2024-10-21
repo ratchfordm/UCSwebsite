42a99d5b2f526df1a3c2fd9c98d00cf65163eebe
Asher Wayde <ashbwayde@gmail.com>
2024-10-21 09:44:47 -0400

    Styling the Admin Page

--- web_src/admin/login.php
+++ web_src/admin/login.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login</title>
+    <link href="../css/global.css" rel="stylesheet">
 </head>
 <body>
+    <div class='logoContainer'>
+        <img src="../images/AdminLogo.png" class="loginLogo" alt="CHAP UCS Admin">
+    </div>
     <h2>Login</h2>
     <form action="./authenticate.php">
+        
+        
         <label for="username">Username:</label><br>
         <input type="text" id="username" name="username"><br><br>
         <label for="pass">Password:</label><br>
