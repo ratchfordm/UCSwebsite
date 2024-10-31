178e20ec43269e3532413810b3824fe85a60a8d0
John McGovern <mcgovernj@etown.edu>
2024-10-31 08:29:03 -0400

    Made a class and delete functionality

--- data_src/db_functions.php
+++ data_src/db_functions.php
@@ -1,98 +1,181 @@
 <?php
 
-    require_once "../../data_src/db_config.php";
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
 
-    $queryDB = function($sql, $type = "s") use ($db) {
-        /*
-        Simply sends any query to the database.
-        */
+    class DatabaseFunctions {
 
-        $stmt = $db->prepare($sql);
-        $stmt->execute();
-
-        $data = $stmt->fetchAll();
-        return $data;
-
-    };
-
-    $insertInto = function($info, $table) use ($db) {
-        /*
-        Inserts something into the database.
-        Automatically parameterizes stuff; just pass an array matching the desired table's columns.
-        Also enforces VARCHAR length limits via substr()
-        */
-
-        if (!strcmp(substr($table, 0, 1), "u")) { // Users
-
-            $data["user_email"] = substr($info[0], 0, 45);
-            $data["user_password"] = substr($info[1], 0, 45);
-            $data["first_name"] = substr($info[2], 0, 45);
-            $data["last_name"] = substr($info[3], 0, 45);
-            $data["admin_level"] = $info[4];
-
-            $sql = $db->prepare("INSERT INTO users (user_email, user_password, first_name, last_name, admin_level) VALUES (?, ?, ?, ?, ?);");
-            $sql->bindParam(1, $data["user_email"], PDO::PARAM_STR);
-            $sql->bindParam(2, $data["user_password"], PDO::PARAM_STR);
-            $sql->bindParam(3, $data["first_name"], PDO::PARAM_STR);
-            $sql->bindParam(4, $data["last_name"], PDO::PARAM_STR);
-            $sql->bindParam(5, $data["admin_level"], PDO::PARAM_INT);
-
-        } else if (!strcmp(substr($table, 0, 1), "c")) { // Categories
-
-            $data["category_descriptions"] = substr($info[0], 0, 30);
-
-            $sql = $db->prepare("INSERT INTO categories (category_description) VALUES (?);");
-            $sql->bindParam(1, $data["category_descriptions"], PDO::PARAM_STR);
-
-        } else if (!strcmp(substr($table, 0, 1), "e")) { // Events
+        private static $db = null;
+        
+        private static function connect() {
             
-            $data["event_name"] = substr($info[0], 0, 45);
-            $data["posting_begin_date"] = $info[1];
-            $data["posting_end_date"] = $info[2];
-            $data["event_begin_date"] = $info[3];
-            $data["event_end_date"] = $info[4];
-            $data["operator_code"] = substr($info[5], 0, 8);
-            
-            $sql = $db->prepare("INSERT INTO events (event_name, posting_begin_date, posting_end_date, event_begin_date, event_end_date, operator_code) VALUES (?, ?, ?, ?, ?, ?);");
-            $sql->bindParam(1, $data["event_name"], PDO::PARAM_STR);
-            $sql->bindParam(2, $data["posting_begin_date"], PDO::PARAM_STR);
-            $sql->bindParam(3, $data["posting_end_date"], PDO::PARAM_STR);
-            $sql->bindParam(4, $data["event_begin_date"], PDO::PARAM_STR);
-            $sql->bindParam(5, $data["event_end_date"], PDO::PARAM_STR);
-            $sql->bindParam(6, $data["operator_code"], PDO::PARAM_STR);
+            if (self::$db === null) {
 
-        } else if (!strcmp(substr($table, 0, 1), "i")) { // Items
+                require_once "../../data_src/db_config.php";
 
-            $data["ISBN"] = $info[0];
-            $data["title"] = substr($info[1], 0, 90);
-            $data["author"] = substr($info[2], 0, 90);
-            $data["price"] = $info[3];
-            $data["year_published"] = $info[4];
-            $data["qty"] = $info[5];
-            $data["donation"] = $info[6];
+                try {
+                    
+                    self::$db = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+                    self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            if (isset($info[7])) $data["sold"] = $info[7];
-            else $data["sold"] = 0;
+                } catch (PDOException $err) {
 
-            $sql = $db->prepare("INSERT INTO items (ISBN, title, author, price, year_published, qty, donation, sold) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-            $sql->bindParam(1, $data["ISBN"], PDO::PARAM_INT);
-            $sql->bindParam(2, $data["title"], PDO::PARAM_STR);
-            $sql->bindParam(3, $data["author"], PDO::PARAM_STR);
-            $sql->bindParam(4, $data["price"]);
-            $sql->bindParam(5, $data["year_published"], PDO::PARAM_INT);
-            $sql->bindParam(6, $data["qty"], PDO::PARAM_INT);
-            $sql->bindParam(7, $data["donation"]);
-            $sql->bindParam(8, $data["sold"]);
+                    echo "CONNECTION FAILED: " . $err->getMessage();
+                    http_response_code(500);
+                    exit();
 
-        } else {
+                }
 
-            echo "Invalid table.";
-            return;
+            }
 
         }
 
-        $sql->execute();
+        public static function queryDB($stmt) {
+            // All-purpose function that sends any statement to the database
+            // Statements must be parameterized beforehand!
 
-    };
+            self::connect();
+
+            $sql = self::$db->prepare($stmt);
+            $sql->execute();
+            $response = $sql->fetchAll();
+
+            return $response;
+
+        }
+
+        public static function insertInto($info, $table) {
+            // Inserts data given as an array into the specified table
+            // Automatically parameterizes queries
+            // Automatically enforces length constraints
+
+            self::connect();
+
+            if (!strcmp(substr($table, 0, 1), "u")) { // Users
+
+                $data["user_email"] = substr($info[0], 0, 45);
+                $data["user_password"] = substr($info[1], 0, 45);
+                $data["first_name"] = substr($info[2], 0, 45);
+                $data["last_name"] = substr($info[3], 0, 45);
+                $data["admin_level"] = $info[4];
+    
+                $sql = self::$db->prepare("INSERT INTO users (user_email, user_password, first_name, last_name, admin_level) VALUES (?, ?, ?, ?, ?);");
+                $sql->bindParam(1, $data["user_email"], PDO::PARAM_STR);
+                $sql->bindParam(2, $data["user_password"], PDO::PARAM_STR);
+                $sql->bindParam(3, $data["first_name"], PDO::PARAM_STR);
+                $sql->bindParam(4, $data["last_name"], PDO::PARAM_STR);
+                $sql->bindParam(5, $data["admin_level"], PDO::PARAM_INT);
+    
+            } else if (!strcmp(substr($table, 0, 1), "c")) { // Categories
+    
+                $data["category_descriptions"] = substr($info[0], 0, 30);
+    
+                $sql = self::$db->prepare("INSERT INTO categories (category_description) VALUES (?);");
+                $sql->bindParam(1, $data["category_descriptions"], PDO::PARAM_STR);
+    
+            } else if (!strcmp(substr($table, 0, 1), "e")) { // Events
+                
+                $data["event_name"] = substr($info[0], 0, 45);
+                $data["posting_begin_date"] = $info[1];
+                $data["posting_end_date"] = $info[2];
+                $data["event_begin_date"] = $info[3];
+                $data["event_end_date"] = $info[4];
+                $data["operator_code"] = substr($info[5], 0, 8);
+                
+                $sql = self::$db->prepare("INSERT INTO events (event_name, posting_begin_date, posting_end_date, event_begin_date, event_end_date, operator_code) VALUES (?, ?, ?, ?, ?, ?);");
+                $sql->bindParam(1, $data["event_name"], PDO::PARAM_STR);
+                $sql->bindParam(2, $data["posting_begin_date"], PDO::PARAM_STR);
+                $sql->bindParam(3, $data["posting_end_date"], PDO::PARAM_STR);
+                $sql->bindParam(4, $data["event_begin_date"], PDO::PARAM_STR);
+                $sql->bindParam(5, $data["event_end_date"], PDO::PARAM_STR);
+                $sql->bindParam(6, $data["operator_code"], PDO::PARAM_STR);
+    
+            } else if (!strcmp(substr($table, 0, 1), "i")) { // Items
+                
+                $data["user_id"] = $info[0];
+                $data["category_id"] = $info[1];
+                $data["event_id"] = $info[2];
+                $data["ISBN"] = $info[3];
+                $data["title"] = substr($info[4], 0, 90);
+                $data["author"] = substr($info[5], 0, 90);
+                $data["price"] = $info[6];
+                $data["year_published"] = $info[7];
+                $data["qty"] = $info[8];
+                $data["donation"] = $info[9];
+    
+                if (isset($info[10])) $data["sold"] = $info[10];
+                else $data["sold"] = 0;
+    
+                $sql = self::$db->prepare("INSERT INTO items (user_id, category_id, event_id, ISBN, title, author, price, year_published, qty, donation, sold) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                $sql->bindParam(1, $data["user_id"], PDO::PARAM_INT);
+                $sql->bindParam(2, $data["category_id"], PDO::PARAM_INT);
+                $sql->bindParam(3, $data["event_id"], PDO::PARAM_INT);
+                $sql->bindParam(4, $data["ISBN"], PDO::PARAM_INT);
+                $sql->bindParam(5, $data["title"], PDO::PARAM_STR);
+                $sql->bindParam(6, $data["author"], PDO::PARAM_STR);
+                $sql->bindParam(7, $data["price"]);
+                $sql->bindParam(8, $data["year_published"], PDO::PARAM_INT);
+                $sql->bindParam(9, $data["qty"], PDO::PARAM_INT);
+                $sql->bindParam(10, $data["donation"], PDO::PARAM_BOOL);
+                $sql->bindParam(11, $data["sold"], PDO::PARAM_BOOL);
+    
+            } else {
+    
+                echo "Invalid table.";
+                return False;
+    
+            }
+    
+            $sql->execute();
+            return True;
+    
+        }
+
+        public static function deleteFrom($table, $column, $condition) {
+
+            self::connect();
+
+            switch (substr($table, 0, 1)) {
+
+                case "u":
+
+                    $tableu = "users";
+                    break;
+
+                case "c":
+
+                    $tableu = "categories";
+                    break;
+
+                case "e":
+
+                    $tableu = "events";
+                    break;
+
+                case "i":
+
+                    $tableu = "items";
+                    break;
+
+                default:
+
+                    echo "Invalid table.";
+                    return False;
+
+            }
+
+            $sql = $db->prepare("DELETE FROM " . $tableu . " WHERE ? LIKE ?;");
+            $sql->bindParam(1, $column);
+            $sql->bindParam(2, $condition);
+
+            $sql->execute();
+            return True;
+
+        }
+
+    }
+
+    $functions = new DatabaseFunctions();
 
 ?>
--- data_src/docs/example.php
+++ data_src/docs/example.php
@@ -1,9 +1,5 @@
 <?php
 
-    ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-    error_reporting(E_ALL);
-
     // Note that including db_config is unnecessary; db_functions does that itself
     require_once "../../data_src/db_functions.php";
 
@@ -12,22 +8,24 @@
 
     echo "SELECT<br><br>";
 
-    $sql = "SELECT * FROM events";
-    $data = $queryDB($sql);
+    $sql = "SELECT * FROM items";
+    $data = $functions->queryDB($sql);
     print_r($data);
 
     // INSERT
-    // Send a basic array of values that correspond to the table's columns
+    // Send a basic array of values that correspond to a table's columns
     // Note that because of unique constraints on various tables insert might fail if you don't come up w/ new info
 
-    echo "<br><br>INSERT<br><br>";
+    echo "<br><br>INSERT<br><i>(Un)comment lines in the code to test this.";
 
-    // WORKING
     // $insertInto(["email", "password", "first name", "last name", 0], "u"); // into users
     // $insertInto(["very epic category"], "c"); // into categories
     // $insertInto(["gang time", "2024-10-24 08:00:00", "2024-10-27 17:00:00", "2024-10-28 07:00:00", "2024-11-2 19:00:00", "mcgovern"], "e"); // into events NOTE YET WORKING
-    
-    // NOT WORKING YET
-    // $insertInto([1234567890000, "title", "author", 1.00, 2012, 100, 0], "i"); // into items
+    // $functions->insertInto([3, 0, 1, 1234567890000, "title", "author", 1.00, 2012, 100, True], "i"); // into items
+
+    // DELETE
+    // Send a column and a condition to a table
+
+    echo "<br><br>DELETE<br><i>(Un)comment lines in the code to test this.";
 
 ?>
