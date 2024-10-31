b7cd92ced9daf53470fd699c3ad64d5bfe042474
Asher Wayde <ashbwayde@gmail.com>
2024-10-31 08:52:13 -0400

    Creating structure for AddItems

--- web_src/user/addItem.php
+++ web_src/user/addItem.php
@@ -12,8 +12,41 @@
     ?>
     <h2>Please tell us some information about your item</h2>
     <form method="post">
-        <label for='title'>Title</label>
-        <input type='text' id='title'>
+        <div>
+            <label for='Author'>Author</label>
+            <input type='text' id='Author'>
+        </div>
+        <div>
+            <label for='title'>Title</label>
+            <input type='text' id='title'>
+        </div>
+        <div>
+            <label for='author'>Author</label>
+            <input type='text' id='author'>
+        </div>
+        <div>
+            <label for='price'>Price</label>
+            <input type='number' id='price'>
+        </div>
+        <div>
+            <label for='year'>Year Published</label>
+            <input type='number' id='year'>
+        </div>
+        <div>
+            <label for='qty'>quantity</label>
+            <input type='number' id='qty'>
+        </div>
+        <div>
+            <label for='donation'>Pick up after UCS?</label>
+            <input type='checkbox' id='donation'>
+        </div>
+        <div>
+            <span>Category Drop down</span>
+        </div>
+        <div>
+            <input type='submit' value='Submit'>
+        </div>
+
     </form>
     <?php
     require_once "../footer.php";
