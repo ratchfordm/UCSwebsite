a2e0be84af6eb4765fe2b3f7ba42370f5fac80e5
John McGovern <mcgovernj@etown.edu>
2024-11-05 10:21:29 -0500

    Created docs page

---
+++ data_src/docs/docs.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Documentation</title>
+</head>
+<body>
+    <h2>-= Database Functions =-</h2>
+    <p>There are several premade functions that can be used to interact with the database.</p>
+    <p>Using these prevents having to reconnect to the actual database every time something is needed, and also reduces technical debt.</p>
+    <h3>queryDB</h3>
+
+</body>
+</html>
