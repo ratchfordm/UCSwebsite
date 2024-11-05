018a31943f6dcc6b0487aea907dd71f7acd32c9b
John McGovern <mcgovernj@etown.edu>
2024-11-05 10:45:43 -0500

    Adding docs for existing functions

--- data_src/docs/docs.php
+++ data_src/docs/docs.php
@@ -9,7 +9,31 @@
     <h2>-= Database Functions =-</h2>
     <p>There are several premade functions that can be used to interact with the database.</p>
     <p>Using these prevents having to reconnect to the actual database every time something is needed, and also reduces technical debt.</p>
-    <h3>queryDB</h3>
-
+    <h3>queryDB($stmt)</h3>
+    <p>Very simple and straigtforward function that takes any SQL statement and executes it.</p>
+    <p><i>You must parameterize your statements beforehand!</i></p>
+    <p>Input(s):</p>
+    <ul>
+        <li>stmt : <i>String</i> -> The statement to be executed.</li>
+    </ul>
+    <p>Output(s):</p>
+    <ul>
+        <li><i>array</i> -> Whatever the SQL statement returned.</li>
+    </ul>
+    <h3>insertInto($info, $table)</h3>
+    <p>This function takes an array of information and inserts it into the specified table.</p>
+    <p>The array's length and content should match the columns of the targeted table.</p>
+    <p>Input(s):</p>
+    <ul>
+        <li>info : <i>array</i> -> The data to be inserted.</li>
+        <li>table : <i>String</i> -> The name of the target table. Technically only one character is needed, as no table's start with the same letter.</li>
+    </ul>
+    <p>Output(s):</p>
+    <ul>
+        <li><i>bool</i> -> Returns True if the insert succeeded; False otherwise.</li>
+    </ul>
+    <h3>deleteFrom($table, $column, $condition)</h3>
+    <p>Deletes data from a specified table.</p>
+    <p>Function is still under construction.</p>
 </body>
 </html>
